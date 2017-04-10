e882a179c1f3f159df88ac524a157b119cd1a5c8
Kelly Gerritz <kelly.gerritz@tufts.edu>
2017-04-09 22:22:11 -0400

    reads random line from file and texts it

--- reminder.rb
+++ reminder.rb
@@ -5,9 +5,16 @@ require 'twilio-ruby'
 account_sid = ARGV[0]
 auth_token = ARGV[1]
 
+chosen_line = nil
+
+File.foreach("reasons.txt").each_with_index do |line, number|
+  chosen_line = line if rand < 1.0/(number+1)
+end
+
+motivation = "Reason to lose weight:\n#{chosen_line}"
+
 @client = Twilio::REST::Client.new account_sid, auth_token
-message = @client.account.messages.create(:body => "Whattup bitch?",
+message = @client.account.messages.create(:body => motivation,
                                           :to => ARGV[2],
                                           :from => ARGV[3])
 
-puts message.sid
