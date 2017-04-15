7624736d0c954601e21a78d288163b1c1088b037
Kelly Gerritz <kelly.gerritz@tufts.edu>
2017-04-15 12:04:35 -0400

    Updated text line

--- reminder.rb
+++ reminder.rb
@@ -11,10 +11,9 @@ File.foreach("reasons.txt").each_with_index do |line, number|
   chosen_line = line if rand < 1.0/(number+1)
 end
 
-motivation = "Reason to lose weight:\n#{chosen_line}"
+motivation = "Reason to lose weight:\n#{chosen_line}\nRemember to sit while eating\n Give yourself credit"
 
 @client = Twilio::REST::Client.new account_sid, auth_token
 message = @client.account.messages.create(:body => motivation,
                                           :to => ARGV[2],
                                           :from => ARGV[3])
-
